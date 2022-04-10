41078e9ba521b0b1c223bf818c481641c4590abe
Ruchit Bhardwaj <ruchitbh@usc.edu>
2022-04-10 01:42:58 -0700

    Email sending introduced for beacon stress signal.

--- public/refugee/beacon.js
+++ public/refugee/beacon.js
@@ -48,8 +48,24 @@ $("#send-text-data").click((event) => {
             help.push(helpObj);
 
             db.collection('users').doc(user.id).update({ helpNeeded: help });
-
         });
+
+        // email sending feature to all those who are helpers and whose email is not the same as the current user email
+        const users1 = db.collection("users");
+        const users2 = users1.where('isHelper', "==", 'true');
+        const query2 = users2.where("email", "!=", email);
+
+        let toList = '';
+        query2.get().then(users => {
+            users.forEach(user => {
+                let userData = user.data();
+                toList += userData.email + "; "
+            });
+            const subject = "Urgent help needed";
+            const body = `THIS IS A CALL FOR URGENT HELP!`;
+            console.log(toList);
+            window.open(`mailto:${toList}?subject=${subject}&body=${body}`);
+        })
     }).catch((error) => {
         console.log(error);
     });
